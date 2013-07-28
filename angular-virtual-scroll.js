0049cec692156b6e583b2f1c28b2c41cac06e15e
pthomas <paul.thomas@s-and-t.com>
2013-07-28 15:06:57 +0100

    Build version 0.5.0

--- angular-virtual-scroll.js
+++ angular-virtual-scroll.js
@@ -1,4 +1,4 @@
-// angular-virtual-scroll - v0.4.0
+// angular-virtual-scroll - v0.5.0
 
 // Include this first to define the module that the directives etc. hang off.
 //
@@ -124,6 +124,8 @@ mod.directive("sfScroller", function(){
   // (part of the sf.virtualScroll module).
   var mod = angular.module('sf.virtualScroll');
   var DONT_WORK_AS_VIEWPORTS = ['TABLE', 'TBODY', 'THEAD', 'TR', 'TFOOT'];
+  var DONT_WORK_AS_CONTENT = ['TABLE', 'TBODY', 'THEAD', 'TR', 'TFOOT'];
+  var DONT_SET_DISPLAY_BLOCK = ['TABLE', 'TBODY', 'THEAD', 'TR', 'TFOOT'];
 
   // Utility to clip to range
   function clip(value, min, max){
@@ -162,26 +164,45 @@ mod.directive("sfScroller", function(){
       };
     }
 
-    // Utility to find the viewport element given the start element
-    function findViewport(startElement){
+    // Utility to filter out elements by tag name
+    function isTagNameInList(element, list){
+      var t, tag = element.tagName.toUpperCase();
+      for( t = 0; t < list.length; t++ ){
+        if( list[t] === tag ){
+          return true;
+        }
+      }
+      return false;
+    }
+
+
+    // Utility to find the viewport/content elements given the start element:
+    function findViewportAndContent(startElement){
       /*jshint eqeqeq:false, curly:false */
       var root = $rootElement[0];
-      var e, n, t, tag;
+      var e, n;
+      // Somewhere between the grandparent and the root node
       for( e = startElement.parent().parent()[0]; e !== root; e = e.parentNode ){
+        // is an element
         if( e.nodeType != 1 ) break;
-        tag = e.tagName.toUpperCase();
-        for( t = 0; t < DONT_WORK_AS_VIEWPORTS.length; t++ ){
-          if( DONT_WORK_AS_VIEWPORTS[t] === tag ) break;
-        }
-        if( t < DONT_WORK_AS_VIEWPORTS.length ) continue;
+        // that isn't in the blacklist (tables etc.),
+        if( isTagNameInList(e, DONT_WORK_AS_VIEWPORTS) ) continue;
+        // has a single child element (the content),
         if( e.childElementCount != 1 ) continue;
+        // which is not in the blacklist
+        if( isTagNameInList(e.firstElementChild, DONT_WORK_AS_CONTENT) ) continue;
+        // and no text.
         for( n = e.firstChild; n; n = n.nextSibling ){
           if( n.nodeType == 3 && /\S/g.test(n.textContent) ){
             break;
           }
         }
         if( n == null ){
-          return angular.element(e);
+          // That element should work as a viewport.
+          return {
+            viewport: angular.element(e),
+            content: angular.element(e.firstElementChild)
+          };
         }
       }
       throw new Error("No suitable viewport element");
@@ -211,11 +232,10 @@ mod.directive("sfScroller", function(){
       viewport.css(viewportCss);
     }
 
-    // Apply explicit styles to the content element.
-    //
+    // Apply explicit styles to the content element to prevent pesky padding
+    // or borders messing with our calculations:
     function setContentCss(content){
       var contentCss = {
-        width:'auto',
         margin: 0,
         padding: 0,
         border: 0,
@@ -226,16 +246,17 @@ mod.directive("sfScroller", function(){
 
     // TODO: compute outerHeight (padding + border unless box-sizing is border)
     function computeRowHeight(element){
-      var style = window.getComputedStyle ? window.getComputedStyle(element) : element.currentStyle,
+      var style = window.getComputedStyle ? window.getComputedStyle(element)
+                                          : element.currentStyle,
           maxHeight = style && style.getPropertyValue('max-height'),
           height = style && style.getPropertyValue('height');
 
-      if( height && height !== '0px' ){
+      if( height && height !== '0px' && height !== 'auto' ){
         $log.info('Row height is "%s" from css height', height);
-      }else if( maxHeight && maxHeight !== '0px' ) {
+      }else if( maxHeight && maxHeight !== '0px' && maxHeight !== 'none' ){
         height = maxHeight;
         $log.info('Row height is "%s" from css max-height', height);
-      }else if( element[0].clientHeight ){
+      }else if( element.clientHeight ){
         height = element.clientHeight+'px';
         $log.info('Row height is "%s" from client height', height);
       }else{
@@ -245,13 +266,11 @@ mod.directive("sfScroller", function(){
       return parseInt(height, 10);
     }
 
-    // The compile gathers information about the declaration. It doesn't make
-    // much sense for separate compile/link as we need a viewport parent that
-    // is exculsively ours.
+    // The compile gathers information about the declaration. There's not much
+    // else we could do in the compile step as we need a viewport parent that
+    // is exculsively ours - this is only available at link time.
     function sfVirtualRepeatCompile(element, attr, linker) {
-      var ident = parseRepeatExpression(attr.sfVirtualRepeat),
-          LOW_WATER = 100,
-          HIGH_WATER = 200;
+      var ident = parseRepeatExpression(attr.sfVirtualRepeat);
 
       return {
         post: sfVirtualRepeatPostLink
@@ -263,14 +282,12 @@ mod.directive("sfScroller", function(){
       // adds a listener to handle child scopes based on the active rows.
       function sfVirtualRepeatPostLink(scope, iterStartElement, attrs){
 
-        var state = 'ngModel' in attrs ? scope.$eval(attrs.ngModel) : {};
         var rendered = [];
         var rowHeight = 0;
         var sticky = false;
-        var viewport = findViewport(iterStartElement);
-        var content = viewport.children();
-
-        // The list structure is controlled by a few simple state variables:
+        var dom = findViewportAndContent(iterStartElement);
+        // The list structure is controlled by a few simple (visible) variables:
+        var state = 'ngModel' in attrs ? scope.$eval(attrs.ngModel) : {};
         //  - The index of the first active element
         state.firstActive = 0;
         //  - The index of the first visible element
@@ -281,11 +298,16 @@ mod.directive("sfScroller", function(){
         state.active = 0;
         // - The total number of elements
         state.total = 0;
+        // - The point at which we add new elements
+        state.lowWater = state.lowWater || 100;
+        // - The point at which we remove old elements
+        state.highWater = state.highWater || 300;
+        // TODO: now watch the water marks
 
-        setContentCss(content);
-        setViewportCss(viewport);
-        // When the user scrolls, we move the state.firstActive
-        viewport.bind('scroll', sfVirtualRepeatOnScroll);
+        setContentCss(dom.content);
+        setViewportCss(dom.viewport);
+        // When the user scrolls, we move the `state.firstActive`
+        dom.viewport.bind('scroll', sfVirtualRepeatOnScroll);
 
         // The watch on the collection is just a watch on the length of the
         // collection. We don't care if the content changes.
@@ -298,9 +320,13 @@ mod.directive("sfScroller", function(){
         // Apply explicit styles to the item element
         function setElementCss (element) {
           var elementCss = {
-            display: 'block',
+            // no margin or it'll screw up the height calculations.
             margin: '0'
           };
+          if( !isTagNameInList(element[0], DONT_SET_DISPLAY_BLOCK) ){
+            // display: block if it's safe to do so
+            elementCss.display = 'block';
+          }
           if( rowHeight ){
             elementCss.height = rowHeight+'px';
           }
@@ -337,11 +363,11 @@ mod.directive("sfScroller", function(){
         function recomputeActive() {
           // We want to set the start to the low water mark unless the current
           // start is already between the low and high water marks.
-          var start = clip(state.firstActive, state.firstVisible - LOW_WATER, state.firstVisible - HIGH_WATER);
+          var start = clip(state.firstActive, state.firstVisible - state.lowWater, state.firstVisible - state.highWater);
           // Similarly for the end
           var end = clip(state.firstActive + state.active,
-                         state.firstVisible + state.visible + LOW_WATER,
-                         state.firstVisible + state.visible + HIGH_WATER );
+                         state.firstVisible + state.visible + state.lowWater,
+                         state.firstVisible + state.visible + state.highWater );
           state.firstActive = Math.max(0, start);
           state.active = Math.min(end, state.total) - state.firstActive;
         }
@@ -352,8 +378,8 @@ mod.directive("sfScroller", function(){
           }
           // Enter the angular world for the state change to take effect.
           scope.$apply(function(){
-            state.firstVisible = Math.floor(evt.target.scrollTop / rowHeight),
-            state.visible = Math.ceil(viewport[0].clientHeight / rowHeight);
+            state.firstVisible = Math.floor(evt.target.scrollTop / rowHeight);
+            state.visible = Math.ceil(dom.viewport[0].clientHeight / rowHeight);
             $log.log('scroll to row %o', state.firstVisible);
             sticky = evt.target.scrollTop + evt.target.clientHeight >= evt.target.scrollHeight;
             recomputeActive();
@@ -437,11 +463,11 @@ mod.directive("sfScroller", function(){
             if( !rowHeight && rendered.length ){
               rowHeight = computeRowHeight(rendered[0][0]);
             }
-            content.css({'padding-top': newValue.start * rowHeight + 'px'});
+            dom.content.css({'padding-top': newValue.start * rowHeight + 'px'});
           }
-          content.css({'height': newValue.len * rowHeight + 'px'});
+          dom.content.css({'height': newValue.len * rowHeight + 'px'});
           if( sticky ){
-            viewport[0].scrollTop = viewport[0].clientHeight + viewport[0].scrollHeight;
+            dom.viewport[0].scrollTop = dom.viewport[0].clientHeight + dom.viewport[0].scrollHeight;
           }
         }
       }
